1c43bf99c3b9d720236104019f8fe33dd8ed9f2f
MazenMDev <mazen.m.mahmoud0@gmail.com>
2025-02-27 15:50:36 +0200

    Version01 of triangle

--- CPP_learn/triangle.cpp
+++ CPP_learn/triangle.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main(){
     int x[20][20];
-    int posr,posc;
+    int posr,posc,posr2,posc2;
     int tot = 0;
     for(int r=0; r<20; r++){
         for(int c=0; c<20; c++){
@@ -10,41 +10,66 @@ int main(){
         }
     }
     
+
+    int g =1;
+    int h = 1;
+    int max = -99999;
+    int posdisplayr,posdisplayc,display_g;
     for(;;){
         for(int r=0; r<20; r++){
             for(int c=0; c<20; c++){
-                posr = r;
-                posc = c;
-                if (true)/*condition*/
+                posr = r+g;
+                posc = c-g;
+                posr2 = r+g;
+                posc2 = c+g;
+                if ((posr<20&&posc>=0) && (posr2<20&&posc2<20))/*condition*/
                 {
                     /* the condition of range*/
                     for(;;){
-                        tot += x[posc][posr];
-                        posr++;
-                        if (posr == c)
+                        tot += x[posr][posc];
+                        posc++;
+                        if (posc == posc2)
                         {
+                            posr--;
                             posc--;
                         }
-                        if (x[posc][posc] == x[r][c])
+                        if (x[posr][posc] == x[r][c])
                         {
+                            tot += x[r][c];
+                            if (tot > max)
+                            {
+                                max = tot;
+                                posdisplayr = r;
+                                posdisplayc = c;
+                                display_g = g;
+                            }
                             break;
                         }
                         
                     }
                 }
-                
             }
+        
         }
-        if((posr+1==4 && posc-1==0 )&&(posr+1==4 && posc+1==4 )){
+        g++;
+        if((posr==19 && posc==0)&&(posr2==19 && posc2==19)){
             break;
         }
     }
 
 
-
-    for(; ;){
-        for(; ;){
-            // the triangle with the largest sum
+int cl = 0;
+    for(int r=posdisplayr ; ; r++){
+        for(int c=posdisplayc-cl ; ; c++){
+            cout << x[r][c] << " ";
+            if (c == (posdisplayc+cl))
+            {
+                break;
+            }
         }
+        cl++;
+        cout << endl;
     }
+
+    cout << "The largest sum: " << max << endl;
 }
