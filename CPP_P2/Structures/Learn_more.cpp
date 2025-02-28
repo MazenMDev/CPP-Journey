d745e84ec2688cb26dbc6c0da7df0c927bc3f431
MazenMDev <mazen.m.mahmoud0@gmail.com>
2025-02-28 19:36:08 +0200

    Friendly use

--- CPP_P2/Structures/Learn_more.cpp
+++ CPP_P2/Structures/Learn_more.cpp
@@ -13,12 +13,15 @@ int main(){
     cin>>N;
     Student*students = new Student[N];
     for(int i=0; i<N; i++){
+        cout << "Enter name of student " << i+1 << ":" ;
         cin>>students[i].name;
+        cout << "Enter GPA of student " << i+1 << ":" ;
         cin>>students[i].gpa;
     }
 
     for(int i=0; i<N; i++){
-        cout << students[i].name << " " << students[i].gpa << endl;
+        cout << "Student " << i+1 << ": ";
+        cout << students[i].name << " |GPA: " << students[i].gpa << endl;
     }
 
     delete[]students;
