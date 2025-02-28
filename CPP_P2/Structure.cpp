26408da037a1998df8f6bc85dcdce4d4fd24f053
MazenMDev <mazen.m.mahmoud0@gmail.com>
2025-02-28 19:11:56 +0200

    add 100 player

--- CPP_P2/Structure.cpp
+++ CPP_P2/Structure.cpp
@@ -1,14 +1,21 @@
+#include <iostream>
+using namespace std;
 struct player
 {
     char name;
-    int rank;
+    int rank[5];
     float salary;
 };
 
 void main(){
     int yi;
-    player p;
-    p.name  = 'a';
-    p.rank = 1;
-    p.salary = 1000.0;
+    player p[100];
+    for(int i=0; i<100; i++){
+        cin>>p[i].name;
+        for(int j=0; j<5; j++){
+            cin>>p[i].rank[j];
+        }
+        cin>>p[i].salary;
+    }
+
 }
