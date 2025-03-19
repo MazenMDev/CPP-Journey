c17ce6b7d9872231d2a77d73a65a7b78b01affc0
MazenMDev <mazen.m.mahmoud0@gmail.com>
2025-03-19 23:40:09 +0200

    fix: resolve initialization errors and bugs

--- Mid-term/structure/first one.cpp
+++ Mid-term/structure/first one.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-struct building 
+struct building
 {
     int flats;
     int card;
@@ -9,64 +9,71 @@ struct building
 };
 
 struct city
-{    
+{
     char name; // assume it is a single letter
     int number_of_b;
-    building B;
+    building *B;
 };
 
 
-int main(){
+int main() {
     int N;
-    cin>>N;
-    city *c = new city[N];
-    int Max = -99999;
-    for(int i=0; i<N ;i++){
-        cin>> c->name;
-        cin>> c->number_of_b;
-        for(int k=0; k< c->number_of_b; k++){
-            cin>>c->B.flats;
-            cin>>c->B.card;
-            cin>>c->B.plastic;
-            cin>>c->B.can
-            if (c[i].B[k].can > Max)
+    cin >> N;
+    city* c = new city[N];
+    int Max_cans = -99999;
+    int Max_flats = -99999;
+    for (int i = 0; i < N; i++) {
+        cin >> c[i].name;
+        cin >> c[i].number_of_b;
+        c[i].B = new building[c[i].number_of_b];
+        for (int k = 0; k < c[i].number_of_b; k++) {
+            cin >> c[i].B[k].flats;
+            cin >> c[i].B[k].card;
+            cin >> c[i].B[k].plastic;
+            cin >> c[i].B[k].can;
+
+            if (c[i].B[k].flats > Max_flats)
             {
-                Max = c[i].B[k].can;
+                Max_flats = c[i].B[k].flats;
+                Max_cans = c[i].B[k].can;
             }
-            
+
         }
     }
 
     char select_c;
-    cin>>select_c; //by name
-    for(int i=0; i<N ;i++){
-        if (select_c == name)
+    cin >> select_c; //by name
+    int save = 0;
+    for (int i = 0; i < N; i++) {
+        if (select_c == c[i].name)
         {
-            for(int k=0; k< c->number_of_b; k++){
-                int save += c->B.plastic;
+            for (int k = 0; k < c[i].number_of_b; k++) {
+                save += c[i].B[k].plastic;
             }
             break;
         }
-        
+
     }
     cout << save << endl;
 
-
-    for(int i=0; i<N ;i++){
-        for(int k=0; k< c->number_of_b; k++){
+    int count = 0;
+    for (int i = 0; i < N; i++) {
+        for (int k = 0; k < c[i].number_of_b; k++) {
             if (c[i].B[k].flats < 10)
             {
-                if (c[i].B[k].can > Max)
+                if (c[i].B[k].can > Max_cans)
                 {
                     count++;
-                    hello wolrd = 0;
                 }
-                
             }
-            
         }
     }
 
     cout << count << endl;
+
+    for (int i = 0; i < N; i++) {
+        delete[] c[i].B;
+    }
+    delete[] c;
 
 }
