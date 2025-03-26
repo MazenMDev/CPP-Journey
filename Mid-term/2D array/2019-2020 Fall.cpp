3ae163b6309493953850a09a384c4c5f1eb79b32
MazenMDev <mazen.m.mahmoud0@gmail.com>
2025-03-26 13:33:14 +0200

    Add: 2016 spring 2D array double pointer

--- Mid-term/2D array/2019-2020 Fall.cpp
+++ Mid-term/2D array/2019-2020 Fall.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 using namespace std;
-
-
+    
 int main(){
     int **x;
     int **y;
@@ -27,6 +26,37 @@ int main(){
     for(r=0; r<m ;r++){
         for(c=0; c< n ;c++){
             cin>>y[r][c];
+        }
+    }
+
+    l = new int[m];
+    z = new int *[m];
+
+    for(int r=0; r<m; r++){
+        ct = 0;
+        for(int c=0; c<n; c++){
+            if (x[r][c] == 0)
+            {
+                if (ct == 0)
+                {
+                    pos1 = c;
+                }
+                else pos2 = c;
+                
+                ct ++;
+            }
+            
+        }
+        len = pos2 - pos1 - 1;
+
+        z[r] = new int [len *z] ;
+        a = 0;
+        l[r] = len *z;
+        for(int c=pos1; c<pos2; c++){
+            z[r][a++] = x[r][c];
+        }
+        for(k=pos1; k<pos2; k++){
+            z[r][a++] = y[r][k];
         }
     }
 }
