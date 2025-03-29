2a4ac0c5b067c11a2c7a968e67a5ecd0e6fcfae6
MazenMDev <mazen.m.mahmoud0@gmail.com>
2025-03-30 00:13:49 +0200

    Fix: erros and make it user friendly

--- CPP_NEW/calculator.cpp
+++ CPP_NEW/calculator.cpp
@@ -1,41 +1,59 @@
 #include <iostream>
 using namespace std;
 
-void menu(){
-
-
-
+void menu() {
+    cout << "___The Calculator___" << endl;
+    cout << "1. Addition (+)" << endl;
+    cout << "2. Subtraction (-)" << endl;
+    cout << "3. Multiplication (*)" << endl;
+    cout << "4. Division (/)" << endl;
+    cout << "5. Exit" << endl;
+    cout << "============================" << endl;
 }
 
-int main(){
+int main() {
     int choice;
-    int num1,num2,result;
-    menu()
+    double num1, num2, result;
     do
     {
+        cout << endl;
+        menu();
+        cout << "Enter the operation: ";
+        cin >> choice;
         if (choice > 0 && choice < 5)
         {
-            switch (choice)
-            {
+            cout << "First number: ";
+            cin >> num1;
+            cout << "Second number: ";
+            cin >> num2;
+            switch (choice) {
             case 1:
-            result = num1 + num2;
+                result = num1 + num2;
+                cout << "Result: " << result << endl;
                 break;
             case 2:
-            result = num1 - num2;
+                result = num1 - num2;
+                cout << "Result: " << result << endl;
                 break;
             case 3:
-            result = num1 * num2;
+                result = num1 * num2;
+                cout << "Result: " << result << endl;
                 break;
             case 4:
-            if (num2 != 0)
-            {
-                result = num1 / num2;
-            }
-                break;
+                if (num2 != 0)
+                {
+                    result = num1 / num2;
+                    cout << "Result: " << result << endl;
+                }
+                else cout << "Error! Division by zero is not allowed." << endl;
+                break;  
             }
         }
-        else if(choice == 5){
-        break;
+        else if (choice == 5) {
+            cout << "Exiting calculator :(" << endl;
         }
+        else cout << "Invalid number.. Try again" << endl;
     } while (choice != 5);
+
+    return 0;
 }
