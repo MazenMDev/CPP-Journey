8606cc1a44cbf28ff63c91fb4427b843d05dc64a
MazenMDev <mazen.m.mahmoud0@gmail.com>
2025-04-11 21:53:26 +0200

    Fix hero position in maze

--- Games/Maze.cpp
+++ Games/Maze.cpp
@@ -21,14 +21,18 @@ void movehero(char move, int& row, int& col, int& ct) {
 }
 
 int main() {
-	char x[20][20];
+	char x[20][20];	
+	int posr, posc;
 	int checkhero = 0;
 	int checktarget = 0;
 	for (int r = 0; r < 5; r++) {
 		for (int c = 0; c < 5; c++) {
 			cin >> x[r][c];
-			if (x[r][c] == '^')
+			if (x[r][c] == '^') {
 				checkhero++;
+				posr = r;
+				posc = c;
+			}
 			if (x[r][c] == '@')
 				checktarget++;
 			if (checkhero > 1)
@@ -62,21 +66,20 @@ int main() {
 	char move;
 	int movever, movehor;
 	int z;
-	int r = 4;
-	int c = 1;	
+	
 	int ct = -1;
-	movever = r;
-	movehor = c;
+	movever = posr;
+	movehor = posc;
 	for (;;) {
 		cin >> move;
-		r = movever;
-		c = movehor;
+		 posr = movever;
+		 posc = movehor;
 		movehero(move, movever, movehor,ct);
 		if (x[movever][movehor] != '#' && movever >= 0 && movever < 5 && movehor >= 0 && movehor < 5)
 		{
 			z = x[movever][movehor];
-			x[movever][movehor] = x[r][c];
-			x[r][c] = z;
+			x[movever][movehor] = x[posr][posc];
+			x[posr][posc] = z;
 		}
 		else 
 		{
