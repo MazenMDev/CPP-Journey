15163e5e89850b5fb1c152b16a75331248e35fcd
MazenMDev <mazen.m.mahmoud0@gmail.com>
2025-04-12 00:27:31 +0200

    Implement Knight (Horse) game with chess rules: move the knight to reach the target

---
+++ Games/Horse chess.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	char x[8][8];
+	for (int r = 0; r < 8; r++) {
+		for (int c = 0; c < 8; c++) {
+			x[r][c] = ' ';
+		}
+	}
+
+	int rowKnight, colKnight;
+	cin >> rowKnight >> colKnight;
+	x[rowKnight][colKnight] = '^';
+	int rowcell, colcell;
+	cin >> rowcell >> colcell;
+	x[rowcell][colcell] = '@';
+
+	int moverow, movecol;
+	int rowdiff, coldiff;
+	for (int r = 0; r < 8; r++) {
+		for (int c = 0; c < 8; c++) {
+			cout << x[r][c] << "|";
+		}
+		cout << endl;
+	}
+	for (;;) {
+		cout << "Enter valid move from (0-7) , (0-7): ";
+		cin >> moverow >> movecol;
+		if (rowKnight > moverow)
+			rowdiff = rowKnight - moverow;
+		else
+			rowdiff = moverow - rowKnight;
+		if (colKnight > movecol)
+			coldiff = colKnight - movecol;
+		else
+			coldiff = movecol - colKnight;
+		if ( ( (moverow >= 0 || moverow < 9) && (movecol >= 0 || movecol < 9) ) && ((rowdiff == 1 && coldiff == 2) || (rowdiff == 2 && coldiff == 1)) )
+		{
+			x[moverow][movecol] = x[rowKnight][colKnight];
+			x[rowKnight][colKnight] = ' ';
+			rowKnight = moverow;
+			colKnight = movecol;
+			for (int r = 0; r < 8; r++) {
+				for (int c = 0; c < 8; c++) {
+					cout << x[r][c] << "|";
+				}
+				cout << endl;
+			}
+		}
+
+		if (rowKnight == rowcell && colKnight == colcell)
+		{
+			cout << "We found the target cell!" << endl;
+			break;
+		}
+	}
+
+}
