2e516ce12c0dfe572c0a140069709ce5cf87be8a
MazenMDev <mazen.m.mahmoud0@gmail.com>
2025-04-11 17:15:04 +0200

    Refactor game logic: Adjust grid size to 10x10, initialize the board, and enhance win condition checks

--- Games/Connect 4.cpp
+++ Games/Connect 4.cpp
@@ -1,18 +1,20 @@
 #include <iostream>
 using namespace std;
 
-
-
-
 int main() {
-	char x[11][11];
+	char x[10][10];
 	int number;
 	int h, red, blue, ct;
 	int gameover = 0;
 	int winr = 0;
 	int winb = 0;
-	for (int i=0; ;i++) {
-		h = 11;
+	for (int r = 0; r < 10; r++) {
+		for (int c = 0; c < 10; c++) {
+			x[r][c] = ' ';
+		}
+	}
+	for (int i = 0; ; i++) {
+		h = 9;
 		red = 0;
 		blue = 0;
 		ct = 0;
@@ -26,6 +28,11 @@ int main() {
 			cout << "Blue Turn: " << endl;
 			blue = 1;
 		}
+		// The above this is correct
+		//
+		//
+
+		//fill the R and B
 		cin >> number;
 		if (x[h][number] != 'R' && x[h][number] != 'B')
 		{
@@ -49,16 +56,24 @@ int main() {
 				x[h][number] = 'B';
 			}
 		}
-		for (int r = 0; r < 11; r++) {
-			for (int c = 0; c < 11; c++) {
-				cout << x[r][c] << ' ';
+		//fill the R and B
+		//
+	
+		//print
+		for (int r = 0; r < 10; r++) {
+			for (int c = 0; c < 10; c++) {
+				cout << x[r][c] << " | ";
 			}
 			cout << endl;
 		}
-		for (int r = 1; r < 11; r++) {
-			for (int c = 1; c < 11; c++) {
-				if (x[r][c] == x[r][c+1] == x[r][c+2] == x[r][c+3])
+		//print
+		//
+
+		for (int r = 0; r < 10; r++) {
+			for (int c = 0; c < 10; c++) {
+				if (x[r][c] == x[r][c + 1] == x[r][c + 2] == x[r][c + 3])
 				{
+					cout << "ver" << endl;
 					if (x[r][c] == 'R')
 						winr = 1;
 					else
@@ -66,8 +81,9 @@ int main() {
 					gameover = 1;
 					break;
 				}
-				if (x[r][c] == x[r + 1][c] == x[r + 2][c] == x[r + 3][c]) 
+				if (x[r][c] == x[r + 1][c] == x[r + 2][c] == x[r + 3][c])
 				{
+					cout << "hor" << endl;
 					if (x[r][c] == 'R')
 						winr = 1;
 					else
@@ -75,24 +91,32 @@ int main() {
 					gameover = 1;
 					break;
 				}
-				if (x[r][c] == x[r + 1][c - 1] == x[r + 2][c - 2] == x[r + 3][c - 3])
-				{
-					if (x[r][c] == 'R')
-						winr = 1;
-					else
-						winb = 1;
-					gameover = 1;
-					break;
+				if (c > 2 && r < 7) {
+					if (x[r][c] == x[r + 1][c - 1] == x[r + 2][c - 2] == x[r + 3][c - 3])
+					{
+						cout << "up right" << endl;
+						if (x[r][c] == 'R')
+							winr = 1;
+						else
+							winb = 1;
+						gameover = 1;
+						break;
+					}
 				}
-				if (x[r][c] == x[r + 1][c + 1] == x[r + 2][c + 2] == x[r + 3][c + 3])
+				if (r < 7 && c < 7)
 				{
-					if (x[r][c] == 'R')
-						winr = 1;
-					else
-						winb = 1;
-					gameover = 1;
-					break;
+					if (x[r][c] == x[r + 1][c + 1] == x[r + 2][c + 2] == x[r + 3][c + 3])
+					{
+						cout << "up left" << endl;
+						if (x[r][c] == 'R')
+							winr = 1;
+						else
+							winb = 1;
+						gameover = 1;
+						break;
+					}
 				}
+
 			}
 			if (gameover == 1) break;
 		}
