3b8fee203f16fd622ef9cd55da0977d7a0ec1263
MazenMDev <mazen.m.mahmoud0@gmail.com>
2025-04-13 14:59:12 +0200

    Improve code readability and organization - Refactor using ternary operator - Add comments for better understanding

--- Games/X-O.cpp
+++ Games/X-O.cpp
@@ -4,85 +4,53 @@ using namespace std;
 int main() {
     char x[3][3];
     int row, col;
-    int X, O;
+
+    // Initialize board
     for (int r = 0; r < 3; r++) {
         for (int c = 0; c < 3; c++) {
             x[r][c] = ' ';
         }
     }
+
     int ch = 0;
+    char currentPlayer;
+
     for (int i = 0; ; i++) {
-        X = 0;
-        O = 0;
-        if (i % 2 == 0) {
-            cout << "X turn" << endl;
-            X = 1;
-        }
-        else {
-            cout << "O turn" << endl;
-            O = 1;
-        }
+        currentPlayer = (i % 2 == 0) ? 'x' : 'o';
+        cout << (currentPlayer == 'x' ? "X turn" : "O turn") << endl;
 
-
+        // Input
         cout << "Choose a (row, col): ";
         cin >> row >> col;
 
-
-
-        if ((x[row][col] != ' ') || (row > 2 || row < 0) || (col > 2 || col < 0))
-        {
-            while ((x[row][col] != ' ') || (row > 2 || row < 0) || (col > 2 || col < 0))
-            {
-                cout << "Wrong select-- Try again: ";
-                cin >> row >> col;
-            }
+        // Input validation
+        while ((row < 0 || row > 2 || col < 0 || col > 2) || x[row][col] != ' ') {
+            cout << "Wrong select-- Try again: ";
+            cin >> row >> col;
         }
 
-        if (X == 1)
-            x[row][col] = 'x';
-        else
-            x[row][col] = 'o';
+        x[row][col] = currentPlayer;
 
-
-
-        cout << endl << x[row][col] << endl;
-
+        // Print board
         for (int r = 0; r < 3; r++) {
             for (int c = 0; c < 3; c++) {
                 cout << x[r][c] << "|";
             }
             cout << endl;
         }
-    
 
+        // Check rows and columns
         for (int i = 0; i < 3; i++) {
-            if (x[i][0] != ' ' && x[i][0] == x[i][1] && x[i][1] == x[i][2]) {
-                ch = 1;
-            }
-            if (x[0][i] != ' ' && x[0][i] == x[1][i] && x[1][i] == x[2][i]) {
-                ch = 1;
-            }
+            if (x[i][0] != ' ' && x[i][0] == x[i][1] && x[i][1] == x[i][2]) ch = 1;
+            if (x[0][i] != ' ' && x[0][i] == x[1][i] && x[1][i] == x[2][i]) ch = 1;
         }
-        if (x[0][0] != ' ' && x[0][0] == x[1][1] && x[1][1] == x[2][2]) {
-            ch = 1;
-        }
-        if (x[0][2] != ' ' && x[0][2] == x[1][1] && x[1][1] == x[2][0]) {
-            ch = 1;
-        }
-        if (ch == 1)
-        {
-            break;
-        }
+
+        // Check diagonals
+        if (x[0][0] != ' ' && x[0][0] == x[1][1] && x[1][1] == x[2][2]) ch = 1;
+        if (x[0][2] != ' ' && x[0][2] == x[1][1] && x[1][1] == x[2][0]) ch = 1;
+
+        if (ch == 1) break;
     }
-    if (X == 1)
-    {
-        cout << "x won" << endl;
-    }
-    if (O == 1)
-    {
-        cout << "o won" << endl;
-    }
+
+    cout << currentPlayer << " won" << endl;
 }
-
-
-
